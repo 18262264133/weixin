2fc1ee0aa85acf393f9f131769d7f82dd821a0a5
xunxiaowei <xunxiaowei@126.com>
2016-12-30 20:31:03 +0800

    project 01

---
+++ project01/pages/logs/logs.js
@@ -0,0 +1,21 @@
+//logs.js
+var util = require('../../utils/util.js')
+Page({
+  data: {
+    logs: []
+  },
+  onShareAppMessage: function () {
+    return {
+      title: '自定义分享标题',
+      desc: '自定义分享描述',
+      path: '/page/user?id=123'
+    }
+  },
+  onLoad: function () {
+    this.setData({
+      logs: (wx.getStorageSync('logs') || []).map(function (log) {
+        return util.formatTime(new Date(log))
+      })
+    })
+  }
+})
---
+++ project01/pages/test/test.js
@@ -0,0 +1,51 @@
+// This is our App Service.
+// This is our data.
+var helloData = {
+  name: 'WeChat'
+}
+
+// Register a Page.
+Page({
+  data: helloData,
+  changeName: function(e) {
+    // sent data change to view
+    this.setData({
+      name: 'MINA'
+    })
+  },
+  viewTap:function(){
+    console.log('view tap');
+  },
+  data: {
+    text: 'init data',
+    array: [{text: 'init data'}],
+    object: {
+      text: 'init data'
+    }
+  },
+  changeText: function() {
+    // this.data.text = 'changed data'  // bad, it can not work
+    this.setData({
+      text: 'changed data'
+    })
+  },
+  changeItemInArray: function() {
+    // you can use this way to modify a danamic data path
+    this.setData({
+      'array[0].text':'changed data'
+    })
+  },
+  changeItemInObject: function(){
+    this.setData({
+      'object.text': 'changed data'
+    });
+  },
+  addNewField: function() {
+    this.setData({
+      'newField.text': 'new data'
+    })
+  },
+  data: {
+    array: [1, 2, 3, 4, 5]
+  }
+})
